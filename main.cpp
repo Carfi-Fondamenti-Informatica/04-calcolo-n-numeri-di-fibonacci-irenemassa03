917288dcbdb95a8bbc50469d019a0e6368093c3f
irenemassa03 <114859857+irenemassa03@users.noreply.github.com>
2022-11-24 21:31:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n, i, a, b, c;
+    cin >> n;
+    a = 1;
+    b = 1;
+    if (n >= 2) {
+        cout << a << endl;
+        cout << b << endl;
+        for (i = 0; i + 2 < n; i++) {
+            c = a + b;
+            cout << c << endl;
+            a = b;
+            b = c;
+        }
+    } else {
+        cout << "errore" << endl;
+
+    }
 }
+
